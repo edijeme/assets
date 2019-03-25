0b31272a99b7d64361a275c1eace92ed42031812
edijeme <edejesusmedina@hotmail.com>
2019-03-25 16:54:00 -0500

    Creacion de modal

--- client/src/app/components/asstes-list/asstes-list.component.ts
+++ client/src/app/components/asstes-list/asstes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 import { AsstesService } from '../../service/asstes.service'
 
@@ -9,22 +9,84 @@ import { AsstesService } from '../../service/asstes.service'
 })
 export class AsstesListComponent implements OnInit {
 
+  @HostBinding('class') classes = 'row';
+
+  resDat: any = [];
   asstes: any = [];
+  assestLoad: any = [];
+  loadAss: any = [];
+  typeAssig: any = [];
+  area: any = [];
+  person: any = [];
+  status: any = [];
+
   constructor(private asstesService: AsstesService) { }
 
   ngOnInit() {
     this.asstesService.getAsstes().subscribe(
       res => {
         this.asstes = res;
-        console.log(this.asstes.data);
-        console.log(Object.keys(this.asstes.data).length);
       },
       err => console.error(err)
     )
   }
 
-  edit(id: string) {
-    console.log(id);
+  load(id: string) {
+    this.asstesService.getSearchAssets(id).subscribe(
+      res => {
+        this.resDat = res;
+        if (this.resDat.status == 200) {
+          this.loadAss = this.resDat.data;
+        }
+      },
+      err => console.error(err)
+    )
+    this.asstesService.getTypeAssig().subscribe(
+      res => {
+        this.resDat = res;
+        if (this.resDat.status == 200) {
+          this.typeAssig = this.resDat.data;
+        }
+      },
+      err => console.error(err)
+    )
+    this.asstesService.getArea().subscribe(
+      res => {
+        this.resDat = res;
+        if (this.resDat.status == 200) {
+          this.area = this.resDat.data;
+        }
+      },
+      err => console.error(err)
+    )
+    this.asstesService.getPerson().subscribe(
+      res => {
+        this.resDat = res;
+        if (this.resDat.status == 200) {
+          this.person = this.resDat.data;
+        }
+      },
+      err => console.error(err)
+    )
+    this.asstesService.getStatus().subscribe(
+      res => {
+        this.resDat = res;
+        if (this.resDat.status == 200) {
+          this.status = this.resDat.data;
+        }
+      },
+      err => console.error(err)
+    )
+  }
+
+  save() {
+    console.log(this.loadAss);
+  }
+
+  changeTypeAss(changeTypeAss, areaTmp, personTmp) {
+    console.log(changeTypeAss);
+    console.log(areaTmp.show);
+    console.log(personTmp);
   }
 
 }
---
+++ client/src/app/models/AssestObj.ts
@@ -0,0 +1,21 @@
+export interface AssestObj {
+    idAssets: number,
+    nameAssets: string,
+    descptAssets: string,
+    typeAssets: string,
+    serialAssets: string,
+    internalNumAssets: string,
+    weightAssets: string,
+    heightAssets: string,
+    widthAssets: string,
+    valueBuyAssets: string,
+    dateBuyAssets: string,
+    dateLowAssets: string,
+    idStatus: string,
+    nameStatus: string,
+    colourAssets: string,
+    idTypeAss: string,
+    nameTypeAss: string,
+    idAsig: string,
+    nameAsig: string
+}
